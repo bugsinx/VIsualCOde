332affd1a4a415e2fcd9c044cafbf16fd24be28e
Yonatan parga <yonatan.parga@devconsultores.com>
2022-10-26 09:59:15 -0500

    Update solution.php

--- codility-tests/findthebug/solution.php
+++ codility-tests/findthebug/solution.php
@@ -1,5 +1,5 @@
 function solution(&$A) {
-	$ans = 0;
+	$ans = 0; //here it's the bug, it should start to check on $A[0]
 	for ($i = 1; $i < sizeof($A); $i++) {
 		if ($ans > $A[$i])
 		    $ans = $A[$i];
